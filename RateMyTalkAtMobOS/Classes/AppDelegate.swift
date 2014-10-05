e4dc2470ce71ea3ddd9ab30a1c8255f70cb3d97d
Bogdan Iusco <bogdan.iusco@gmail.com>
2014-10-05 12:24:58 +0300

    Add CoreData logger.

--- RateMyTalkAtMobOS/Classes/AppDelegate.swift
+++ RateMyTalkAtMobOS/Classes/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         runStarters()
         
         RMTCloudKitManager.sharedInstance.downloadAll { () -> Void in
+            RMTLogCoreData.printAllSpeakers()
         }
         return true
     }
---
+++ RateMyTalkAtMobOS/Classes/Helpers/Logger/RMTLogCoreData.swift
@@ -0,0 +1,49 @@
+//
+//  RMTLogCoreData.swift
+//  RateMyTalkAtMobOS
+//
+//  Created by Bogdan Iusco on 10/5/14.
+//  Copyright (c) 2014 Grigaci. All rights reserved.
+//
+
+import Foundation
+
+class RMTLogCoreData {
+    class func printAllSessions() {
+        var allSessions: NSArray = RMTSession.MR_findAll()
+        for sessionObj in allSessions {
+            let session: RMTSession = sessionObj as RMTSession
+            println("=====================\(session.title?)=====================")
+            printSpeaker(session.speaker)
+            let allRatingCategories:NSOrderedSet = session.ratingCategories
+            let count = allRatingCategories.count - 1
+            if count > 0 {
+                for i in 0...count {
+                    let ratingCategory: RMTRatingCategory = allRatingCategories.objectAtIndex(i) as RMTRatingCategory
+                    println("--")
+                    printRatingCategory(ratingCategory)
+                    println("--")
+                }
+            }
+            println("==========================================")
+        }
+    }
+    
+    class func printSpeaker(speaker: RMTSpeaker?) {
+        println("speaker: \(speaker?.name)")
+        println("recordID: \(speaker?.ckRecordID)")
+    }
+
+    class func printRatingCategory(ratingCategory: RMTRatingCategory?) {
+        println("title: \(ratingCategory?.title)")
+        println("detail: \(ratingCategory?.detail)")
+    }
+    
+    class func printAllSpeakers() {
+        var allSpeakers: NSArray = RMTSpeaker.MR_findAll()
+        for sessionObj in allSpeakers {
+            let speaker: RMTSpeaker = sessionObj as RMTSpeaker
+            printSpeaker(speaker)
+        }
+    }
+}
