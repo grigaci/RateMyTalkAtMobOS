115c7541b5cec8f9d123590408db2607ef75b51e
Bogdan Iusco <bogdan.iusco@gmail.com>
2014-10-12 12:26:13 +0300

    User defaults extension for saving an user uuid.

---
+++ RateMyTalkAtMobOS/Classes/Helpers/Extensions/NSUserDefaults+UUID.swift
@@ -0,0 +1,24 @@
+//
+//  NSUserDefaults+UUID.swift
+//  RateMyTalkAtMobOS
+//
+//  Created by Bogdan Iusco on 10/12/14.
+//  Copyright (c) 2014 Grigaci. All rights reserved.
+//
+
+import Foundation
+
+extension NSUserDefaults {
+    var userUUID: String {
+        get
+        {
+            let uuid = self.objectForKey("userUUID") as? String
+            if uuid == nil {
+                let newUUID = NSUUID.UUID().UUIDString
+                self.setObject(newUUID, forKey: "userUUID")
+                return newUUID
+            }
+            return uuid!
+        }
+    }
+}
