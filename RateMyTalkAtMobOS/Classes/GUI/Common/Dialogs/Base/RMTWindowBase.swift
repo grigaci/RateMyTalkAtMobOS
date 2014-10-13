ab9fa9ad15e662c5449996dbaf63b013eb0f120b
Bogdan Iusco <bogdan.iusco@gmail.com>
2014-10-13 19:55:01 +0300

    Add base window class.

---
+++ RateMyTalkAtMobOS/Classes/GUI/Common/Dialogs/Base/RMTWindowBase.swift
@@ -0,0 +1,66 @@
+//
+//  RMTWindowBase.swift
+//  RateMyTalkAtMobOS
+//
+//  Created by Bogdan Iusco on 10/13/14.
+//  Copyright (c) 2014 Grigaci. All rights reserved.
+//
+
+import UIKit
+
+class RMTWindowBase: UIWindow {
+    var lastWindow: UIWindow?
+
+    override init() {
+        let frame = UIScreen.mainScreen().bounds
+        super.init(frame: frame)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(viewController: UIViewController) {
+        super.init()
+        self.rootViewController = viewController
+    }
+    
+    func show() {
+        self.lastWindow = UIApplication.sharedApplication().keyWindow
+        self.windowLevel = self.lastWindow!.windowLevel + 1.0
+        self.makeKeyAndVisible()
+        
+        RMTWindowBase.registerWindow(self)
+    }
+    
+    func hide() {
+        self.windowLevel = self.lastWindow!.windowLevel - 1.0
+        self.lastWindow?.makeKeyAndVisible()
+        RMTWindowBase.unregisterWindow(self)
+    }
+
+     class var aliveWindows: NSMutableArray {
+     struct Static {
+        static var onceToken : dispatch_once_t = 0
+        static var array : NSMutableArray? = nil
+        }
+        dispatch_once(&Static.onceToken) {
+            Static.array = NSMutableArray()
+        }
+        return Static.array!
+    }
+
+    class func registerWindow(window: UIWindow) {
+        RMTWindowBase.aliveWindows.addObject(window)
+    }
+
+    class func unregisterWindow(window: UIWindow) {
+        RMTWindowBase.aliveWindows.removeObject(window)
+    }
+}
