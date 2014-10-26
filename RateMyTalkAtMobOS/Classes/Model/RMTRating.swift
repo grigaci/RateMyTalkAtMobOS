8e27bc18a6fe3f59b067a1fbb7cc92649a4270af
Bogdan Iusco <bogdan.iusco@gmail.com>
2014-10-26 15:05:56 +0200

    Use thread context for deleting the ratings.

--- RateMyTalkAtMobOS/Classes/Model/RMTRating.swift
+++ RateMyTalkAtMobOS/Classes/Model/RMTRating.swift
@@ -5,10 +5,11 @@ class RMTRating: _RMTRating {
         
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
             let userUUID = NSUserDefaults.standardUserDefaults().userUUID
-            let moc: NSManagedObjectContext = NSManagedObjectContext.MR_defaultContext()
+            let moc: NSManagedObjectContext = NSManagedObjectContext.MR_contextForCurrentThread()
             let predicate = NSPredicate(format: "%K != %@", RMTRatingAttributes.userUUID.rawValue, userUUID)
             var objectsToDelete: [RMTRating] = RMTRating.MR_findAllWithPredicate(predicate, inContext: moc) as [RMTRating]
             for rating in objectsToDelete {
+                rating.ratingCategory?.removeRatingsObject(rating)
                 moc.deleteObject(rating)
             }
             
